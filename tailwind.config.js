293e795db55f627d097d616dc51a30092a2cd0f7
FUMIPO <fumiponash@gmail.com>
2025-05-15 11:55:33 +0900

    依存関係の更新、PostCSSおよびTailwind CSSの設定ファイルの修正、不要なファイルの削除を実施

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       textShadow: {
-        DEFAULT: '1px 1px 2px rgba(0, 0, 0, 0.5)',
+        DEFAULT: "1px 1px 2px rgba(0, 0, 0, 0.5)",
       },
     },
   },
   plugins: [],
-} 
+};
